6526bf2729c2a1f59f4f693b402ce27ee2dfbc33
Justfemi <ajibade.olufemi@yahoo.com>
2024-07-22 20:00:42 +0100

    feat: firebase configurations

---
+++ firebase.ts
@@ -0,0 +1,8 @@
+import { initializeApp } from 'firebase/app';
+import { clientConfig } from './config';
+import { getAuth } from 'firebase/auth';
+
+const app = initializeApp(clientConfig);
+const auth = getAuth(app)
+
+export { app, auth };
