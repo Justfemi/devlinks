3643a4a7aa934ba34d4fb23c5e149dfc7f1cb0de
Justfemi <ajibade.olufemi@yahoo.com>
2024-07-26 14:02:15 +0100

    ch:modified firebase config for storage

--- firebase.ts
+++ firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { clientConfig } from './config';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from '@firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { getStorage } from '@firebase/storage';
 
 const app = initializeApp(clientConfig);
 const auth = getAuth(app)
